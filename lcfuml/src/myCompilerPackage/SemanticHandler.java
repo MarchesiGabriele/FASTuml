ff0a3642e17c3dfc802a20633b65781d85a34282
Cristian <tironi18@gmail.com>
2024-10-07 16:11:33 +0200

    maven upgrade

--- lcfuml/src/myCompilerPackage/SemanticHandler.java
+++ lcfuml/src/myCompilerPackage/SemanticHandler.java
@@ -3,7 +3,23 @@ package myCompilerPackage;
 import java.util.ArrayList;
 import java.util.Hashtable;
 
+import myCompilerPackage.util.Variable;
+
 public class SemanticHandler {
+	
+	// ****** codici per i messaggi d'errore
+	public static int LEXICAL_ERROR 		= 0;
+	public static int SYNTAX_ERROR 			= 1;
+	
+	// ??
+	public static int FIRST_WARNING 		= 100;
+	public static int NO_REF_WARNING 		= 101;
+	
+	// ****** attributi semantici globali
+	Hashtable<String, Variable> symbolTable;
+	ArrayList<String> errors;
+	ArrayList<String> warnings;
+		
 	public SemanticHandler () {
 		
 	}
